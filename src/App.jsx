6dd5f603587c654c8437160f1265811a43c69638
00iun <lsh681236@naver.com>
2025-01-05 01:04:44 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import "./App.scss";
+// import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+import { flexMixin } from "./components/styled"
+
+// 1. `npm install styled-components` 를 터미널에 입력하여 설치합니다.
+// 2. `npm install & npm run dev` 를 터미널에 입력하여 서버를 실행합니다.
+// 3. `import styled from "styled-components"` 를 파일에 불러온 후 사용할 수 있습니다.
+// 4. `App.scss` 에 작성된 코드를 각 `className`에 해당하는 요소로 이동하여 Styled-Components를 사용하여 새롭게 작성해 주세요.
+//     1. `App.jsx`
+//     2. `components/Content.jsx`
+//     3. `components/Header.jsx`
+// 5. css는 자유롭게 수정해도 좋습니다.
 
 function App() {
+  const StyledSection = styled.section`
+  section {
+    ${flexMixin({ justify: 'center', align: 'center', wrap: 'wrap', gap: '20px' })};
+    padding: 20px 40px;
+  }
+`
+
   return (
     <main>
       <Header />
-      <section>
+      <StyledSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </StyledSection>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,30 @@
+import styled from "styled-components"
+import { flexMixin, fontMixin } from "./styled"
+
 export default function Header() {
+  const StyledHeader = styled.header`
+  .header-container {
+    ${flexMixin({ justify: 'space-between', align: 'center' })};
+    padding: 20px 20px;
+    background-color: black;
+    ul {
+      ${flexMixin({ justify: 'center', align: 'center', gap: '20px' })};
+      li {
+        ${fontMixin({ size: '16px', weight: 400 })};
+        list-style: none;
+      }
+    }
+  }
+`
+
   return (
-    <header className="header-container">
+    <StyledHeader className="header-container">
       <h1>OZ코딩스쿨</h1>
       <ul className="menu-container">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
       </ul>
-    </header>
+    </StyledHeader>
   );
 }
